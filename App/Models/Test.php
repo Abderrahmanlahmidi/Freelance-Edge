51668706157ccd86b28cafc9e96daa89ff5011ab
lahmidi <dvabderrahman@gmail.com>
2025-02-07 20:23:55 +0100

    create connection pgdb

--- App/Models/Test.php
+++ App/Models/Test.php
@@ -1,2 +1,13 @@
 <?php
-echo "Test Model";
+require basePath('App/Database/DatabaseConnection.php');
+
+class Test {
+
+    public function getTest(){
+        $query = "SELECT * FROM test";
+        $stmt = DatabaseConnection::getInstance()->prepare($query);
+        $stmt->execute();
+        $data =  $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $data;
+    }
+}
--- Schema.sql
+++ Schema.sql
@@ -1,1 +1,17 @@
---query her
+--install the postgre and run this commands in the query in the postgree shell
+CREATE DATABASE FreelanceEdgeDB;
+-- List all databases to check if it was created successfully: \l
+-- use database: \c FreelanceEdgeDB;
+-- create table test in the database:
+CREATE TABLE test (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INT CHECK (age >= 0),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+-- Check if the table was created successfully: \d test
+-- To test your table, insert some data:
+INSERT INTO test (name, age) VALUES ('Alice', 25), ('Bob', 30);
+-- Retrieve all rows from the table:
+SELECT * FROM test;
+
