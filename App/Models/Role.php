5a43d334d9d5cb10caf5762d3325e646a727fd85
unknown <zz240614@gmail.com>
2025-02-08 01:33:42 +0100

    Creted Role , Contrat and Message Classes With attribuit an dconstruct and Getters and setters and method function to string

---
+++ App/Models/Role.php
@@ -0,0 +1,60 @@
+<?php
+
+
+class Role
+{
+    private int $id = 0;
+    private string $rolename = "";
+
+
+
+    public function __construct() {}
+
+
+
+    public static function instanceWithName(string $name): Role
+    {
+        $instance = new self();
+
+        $instance->rolename = $name;
+
+        return $instance;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setRoleName(string $rolename): void
+    {
+        $this->rolename = $rolename;
+    }
+
+
+
+
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getRoleName(): string
+    {
+        return $this->rolename;
+    }
+
+
+
+
+
+    public function __toString()
+    {
+        $id = $this->id;
+        $name = $this->rolename;
+
+
+        return "(Role) => id : " . $id . " , Role name : " . $name;
+    }
+}
